a82ea4cda8e6a3afda59012d24910606a06e7b0a
Alexander D Brown <brownal7@uk.ibm.com>
2018-04-19 13:25:13 +0100

    Use in-memory and TSQ storage to contrast the services.

--- projects/com.ibm.cicsdev.osgi.ds.tsq_1.0.1/src/com/ibm/cicsdev/osgi/ds/tsq/TemporaryStorageQueueService.java
+++ projects/com.ibm.cicsdev.osgi.ds.tsq_1.0.1/src/com/ibm/cicsdev/osgi/ds/tsq/TemporaryStorageQueueService.java
@@ -8,32 +8,41 @@ import com.ibm.cicsdev.osgi.ds.TemporaryStorageService;
 
 public class TemporaryStorageQueueService implements TemporaryStorageService {
 	private static final String TSQ_NAME = "TSQS";
-	
+
+	public void activate() {
+		System.out.println("Starting TSQ service");
+	}
+
+	public void deactivate() {
+		System.out.println("Stopping TSQ service");
+	}
+
 	private static TSQ getTSQ() {
 		TSQ tsq = new TSQ();
 		tsq.setName(TSQ_NAME);
-		
+
 		return tsq;
 	}
-	
+
 	@Override
 	public String get(int id) throws TemporaryStorageException {
 		final ItemHolder holder = new ItemHolder();
-		
+
 		try {
 			getTSQ().readItem(id, holder);
-		} catch(CicsConditionException e) {
+		} catch (CicsConditionException e) {
 			throw new TemporaryStorageException(e, e.getRESP(), e.getRESP2());
 		}
-		
+
 		return holder.getStringValue();
 	}
-	
+
 	@Override
 	public int put(String record) throws TemporaryStorageException {
 		try {
-			return getTSQ().writeString(record);
-		} catch(CicsConditionException e) {
+			final int id = getTSQ().writeString(record);
+			return id;
+		} catch (CicsConditionException e) {
 			throw new TemporaryStorageException(e, e.getRESP(), e.getRESP2());
 		}
 	}
